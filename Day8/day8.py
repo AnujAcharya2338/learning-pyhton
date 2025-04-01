e3b74452917c29a07331149f9454f360dcc70de7
AnujAcharya2338 <anujacharya877.com>
2025-04-01 09:03:51 -0700

    functions with arguments

---
+++ Day8/day8.py
@@ -0,0 +1,16 @@
+# def great():
+#     print("Hello")
+#     print("World")
+#     print("Office")
+    
+# great()
+
+# def greet(name):
+#     print(f"Hello {name}" )
+#     print(f"How you doing {name}")
+    
+# greet("Anuj")
+    
+def great_With(name , address):
+    print(f"Hello {name} \n  What is it like in {address}")
+great_With("Anuj","Taudaha")    
