acf13e4dabfe3fa3085c5476447f053f4cea8d66
AnujAcharya2338 <anujacharya877@gmail.com>
2025-05-02 08:55:37 -0700

    Dimag kharab

---
+++ Day11/practice.py
@@ -0,0 +1,67 @@
+
+# import random
+# def random_cards():
+#     cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+#     card=random.choice(cards)
+#     return card
+# # playgame=input("Do you want to play a game og Blackjack? Type 'y' or 'n': ")
+# # from art import logo
+# human_cards=[]
+# computer_card=[]
+# human_cards.append(random_cards())
+# human_cards.append(random_cards())
+# print(f"Your cards:{human_cards}  current score:{human_cards[0] + human_cards[1]}")
+# computer_card.append(random_cards())
+# print(f"Computer's first card:{computer_card}")
+# add_cards=input("Type 'y' to  get another card, type 'n' to pass:")
+# while add_cards == "y":
+#     human_cards.append(random_cards())
+#     total=sum(human_cards)
+#     print(f"Your cards:{human_cards}  current score:{sum}")
+#     print(f"Computer's first card:{computer_card}")
+#     if sum < 
+#     print(f"Your final hand{human_cards} final score:{sum}")
+#     print(f"computer's final hand:{computer_card}, final score: {}")
+
+#################################################################################
+
+# import random
+# def cards():
+#     cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+#     card=random.choice(cards)
+#     return cards
+
+
+# def calculate_score():
+#     user_cards=[]
+#     computer_cards=[]
+#     user_cards.append(cards())
+#     user_cards.append(cards())
+#     total_user=sum(user_cards)
+#     computer_cards.append(cards())
+#     computer_cards.append(cards())
+#     total_computer=sum(user_cards)
+#     return total_user, total_computer
+
+# if total_user == 21 and len(user_cards == 2):
+#     return 0
+# if 11 in cards and total_user>21:
+#     user_cards.remove(11)
+#     user_cards.append(1)
+#     total=sum(user_cards)
+# elif total_user>21:
+#     print("You lose")
+# else:
+#     add_cards=input("Do you want to get another card? y or no?")
+# if add_cards == "y":
+#     calculate_score()
+# else:
+            
+            
+        
+        
+        
+        
+    
+
+    
