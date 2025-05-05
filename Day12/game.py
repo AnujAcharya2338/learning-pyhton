5da2f15bbc2f401b20cfdaab62e65e2478d9f13d
AnujAcharya2338 <anujacharya877@gmail.com>
2025-05-05 02:50:03 -0700

    Number Guessing Game Complete. Not the very best way to do it but just managed to do it.

--- Day12/game.py
+++ Day12/game.py
@@ -10,43 +10,56 @@ def random_number():
         total_numbers.append(number)
     return random.choice(total_numbers)
 
-desired_number=random_number()
-print(desired_number)
 
-def high(guess):
+def high(attempts):
     print("Too high.")
     print("Guess again.")
-    choices(guess)
+    return attempts-1
 
-def low(guess):
+
+def low(attempts):
     print("Too low.")
     print("Guess again.")
-    choices(guess)
+    return attempts-1
 
-def won(guess):
-    print(f"You got it! The answer was {desired_number}")
 
-def difficulty(game_difficulty):
-    print(f"You have {game_difficulty} attempts remaining to guess a number.")
-    guess = input("Make a guess: ")
-    choices(guess)
-    
-    
-def choices(guess):
-    if guess > desired_number:
-        high(guess)
-    elif guess < desired_number:
-        low(guess)
-    elif guess == desired_number:
-        won(guess)
+def won():
+    return print(f"You got it! The answer was {desired_number}")
+
+
+def choices(guess_number, attempts):
+    if guess_number > desired_number:
+        return high(attempts)
+    elif guess_number < desired_number:
+        return low(attempts)
+    else:
+        won()
+        return -1
+
+def guess(game_difficulty):
+    attempts = game_difficulty
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess a number.")
+        guess_number = int(input("Make a guess: "))
+        result=choices(guess_number, attempts)
+        if result == -1:
+            return
+        else:
+            attempts = result
+    print(f"You have run out of guesses.The number was {desired_number}.")    
+
 
 def play_game():
     print("Welcome to the Number Guessing Game!")
     print("I am thinking of a number between 1 and 100")
     game_choice = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if game_choice == "hard":
-        difficulty(5)
+        guess(5)
     else:
-        difficulty(10)
-        
+        guess(10)
+
+
+desired_number = random_number()
+print(desired_number)
+
 play_game()
