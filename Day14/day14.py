0035934016bbdf135e43e8787c52398339dad429
AnujAcharya2338 <anujacharya877@gmail.com>
2025-05-17 08:54:07 -0700

    RAhhhh

--- Day14/day14.py
+++ Day14/day14.py
@@ -3,33 +3,33 @@ from art import vs
 from names import data
 import random
 
-
-
 length=len(data)
 
-for _ in range(2):
-    index1=random.randint(0,length)
-    index2=random.randint(0,length)
+count = 0
+game_end = False
+index1=random.randint(0,length-1)
+index2=random.randint(0,length-1)
 print(logo)
-
 
 choice1=data[index1]
 choice2=data[index2]
 
 def compare(a_followers,b_followers):
     if a_followers > b_followers:
-        return a_followers
+        return 'A'
     else:
-        return b_followers
+        return 'B'
     
 print(f"Compare A: {choice1['name']}, a {choice1['description']}, from {choice1['country']}")
 print(vs)
 print(f"Compare B: {choice2['name']}, a {choice2['description']}, from {choice2['country']}")
 
-aorb=int(input("Who haas more followers?? 'A' or 'B'"))
+aorb=input("Who has more followers?? 'A' or 'B'.").strip().upper()
 
 correct_ans=compare(choice1['follower_count'],choice2['follower_count'])
-if aorb == correct_ans == True:
-    print(f"You are right")
+
+if aorb == correct_ans:
+    print(f"You are right")  
+    count +=1
 else:
     print("You are wrong")
