cc64993fbe0a9fa8a72bd8649b69005b21e45586
AnujAcharya2338 <anujacharya877@gmail.com>
2025-06-10 06:17:51 -0700

    TYpe A with turtle

---
+++ Day18/day18.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.left(45)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(180)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(45)
+timmy_the_turtle.forward(130)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
