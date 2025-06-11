010b8ddda98f09079107d7aadbcd9cf767bb5897
AnujAcharya2338 <anujacharya877@gmail.com>
2025-06-11 09:59:29 -0700

    Bombocla

--- Day18/day18.py
+++ Day18/day18.py
@@ -1,14 +1,19 @@
 from turtle import Screen as s, Turtle as t
-
+import random
 tim = t()
 tim.shape("turtle")
 tim.color("red")
 
-for _ in range(50): 
-     
-  tim.forward(10)
-  tim.penup()
-  tim.forward(10)
-  tim.pendown()
+colors = ["pale green", "aquamarine", "orange red" , "saddle brown", "goldenrod" ,"light sea green","deep sky blue","salmon","dark goldenrod","dark slate blue"]
+def shape(num_side):
+  for _ in range(num_side):
+   angle=360/num_side
+   tim.forward(70)
+   tim.left(angle)
+ 
+ 
+for num_size in range(3,11):
+     tim.color(random.choice(colors))
+     shape(num_size)
 screen = s()
 screen.exitonclick()
