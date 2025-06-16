e1321a518755a6309837b33a2e8622c16c319a63
AnujAcharya2338 <anujacharya877@gmail.com>
2025-06-16 09:50:33 -0700

    bYE bYE WINDOWS

--- Intermidate/Day20/day20.py
+++ Intermidate/Day20/day20.py
@@ -8,28 +8,19 @@ screen.title("Snake game")
 screen.tracer(0)
 
 snake = Snake()
-snake=[]
-x_axis=[0,-20,-40]
-for position in range(0,3):
-    half_snake=Turtle()
-    half_snake.shape("square")
-    half_snake.color("white")
-    half_snake.penup()
-    half_snake.goto(x=x_axis[position],y=0)
-    snake.append(half_snake)
 
+screen.listen()
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down,"s")
+screen.onkey(snake.left,"a")
+screen.onkey(snake.right,"d")
 game_is_on= True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
-    for seg in range(len(snake)-1,0,-1):
-        x_cordinate=snake[seg-1].xcor()
-        y_cordinate=snake[seg-1].ycor()
-        snake[seg].goto(x_cordinate,y_cordinate)
-    snake[0].forward(20)
 
+    snake.move()
 
 
 
--- Intermidate/Day20/snake.py
+++ Intermidate/Day20/snake.py
@@ -1,3 +1,32 @@
+from turtle import Turtle
+STARTING_POSITION=[0,-20,-40]
+MOVEMENT = 20
 class Snake:
     def __init__(self):
+        self.snake=[]
+        self.create_snake()
+    
+    def create_snake(self):
+        for position in range(0,3):
+            half_snake=Turtle()
+            half_snake.shape("square")
+            half_snake.color("white")
+            half_snake.penup()
+            half_snake.goto(x=STARTING_POSITION[position],y=0)
+            self.snake.append(half_snake)
         
+    def move(self):   
+        for seg in range(len(self.snake)-1,0,-1):
+            x_cordinate=self.snake[seg-1].xcor()
+            y_cordinate=self.snake[seg-1].ycor()
+            self.snake[seg].goto(x_cordinate,y_cordinate)
+        self.snake[0].forward(MOVEMENT)
+    
+    def up(self):
+        self.snake[0].setheading(90)
+    def down(self):
+        self.snake[0].setheading(270)
+    def left(self):
+        self.snake[0].setheading(180)
+    def right(self):
+        self.snake[0].setheading(0)
